a06a61f23b55e3291f2c868cea959b2d31fc45be
tellay <tellayprivadobuilds@gmail.com>
2024-01-26 15:08:31 +0000

    chore: formatting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,13 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
+import { Layout } from "~/components/layout";
+import { Sidebar } from "~/components/sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <Button>Vite</Button>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <Button variant={"outline"}>React</Button>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Sidebar />
+        <p>Main</p>
+      </Layout>
     </>
   );
 }
