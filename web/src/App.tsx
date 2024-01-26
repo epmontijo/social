5eda423e81cbe0b4b9c85fe8ef0d06524338d681
tellay <tellayprivadobuilds@gmail.com>
2024-01-26 14:31:36 +0000

    feat: add tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +7,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          Vite
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          React
         </a>
       </div>
       <h1>Vite + React</h1>
